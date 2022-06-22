7615a74f37f0a69ce8173feca76c0e1a4a5224cc
weshy <josephwaweru96@gmail.com>
2022-06-22 15:03:42 +0300

    Resetting password views

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,28 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include 
 
 from . import views
 
 urlpatterns = [ 
     # path('login/', views.user_login, name='login'),
+    path('', views.dashboard, name='dashboard'),
 
+    # LOGIN && LOGOUT URLS
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
+    # CHANGE PASSWORD URLS
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
-    path('', views.dashboard, name='dashboard'),
+    # ReSETTING YOUR PASSWORD URLS
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    #  Django Authentication Modules
+    # path('', include('django.contrib.auth.urls')),
+    
     
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
